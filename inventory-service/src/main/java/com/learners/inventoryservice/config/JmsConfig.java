348695499924a6d4529935c6e915c7b4a6d2531d
yashinaa <yashinaa>
2022-03-20 16:21:15 +0400

    Added activemq config

---
+++ inventory-service/src/main/java/com/learners/inventoryservice/config/JmsConfig.java
@@ -0,0 +1,21 @@
+package com.learners.inventoryservice.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+
+    @Bean
+    public MessageConverter messageConverter(ObjectMapper mapper) {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setObjectMapper(mapper);
+        converter.setTargetType(MessageType.TEXT);
+        return converter;
+    }
+
+}
