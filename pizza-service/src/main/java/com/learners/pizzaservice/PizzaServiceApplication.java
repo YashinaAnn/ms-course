e7d6da8c4e7e0fb901caf2355897ac45339f4c01
Anna Yashina <ayashina@Annas-MacBook-Pro.local>
2022-09-05 10:11:10 +0400

    Configured Eureka Clients

--- pizza-service/src/main/java/com/learners/pizzaservice/PizzaServiceApplication.java
+++ pizza-service/src/main/java/com/learners/pizzaservice/PizzaServiceApplication.java
@@ -3,7 +3,9 @@ package com.learners.pizzaservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @EnableCaching
 @SpringBootApplication
 public class PizzaServiceApplication {
