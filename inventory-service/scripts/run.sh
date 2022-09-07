5fc1b92fb5c8392dab1770e8b7ad7d0413605e2b
Anna Yashina <ayashina@Annas-MacBook-Pro.local>
2022-09-07 18:36:07 +0400

    Added docker compose file

--- inventory-service/scripts/run.sh
+++
@@ -1,8 +0,0 @@
-docker run -d -e POSTGRES_USER=local -e POSTGRES_PASSWORD=root -e POSTGRES_DB=pizza_db -p 5432:5432  postgres
-
-docker run -d -it --rm \
-  -p 8161:8161 \
-  -p 61616:61616 \
-  vromero/activemq-artemis
-
-docker run -d -p 9411:9411 openzipkin/zipkin
